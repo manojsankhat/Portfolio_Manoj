d6e38abce96624ca59f049a1ae85d28ace47025e
Manoj Sankhat <manoj.sankhatt@gmail.com>
2022-11-22 08:25:30 +0530

    commit after implemantation of camara option in Food Mart

--- app/src/main/java/com/example/testapp/activities/ActivityVendorFoodItems.java
+++ app/src/main/java/com/example/testapp/activities/ActivityVendorFoodItems.java
@@ -1,14 +1,26 @@
 package com.example.testapp.activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.testapp.FoodMartApp;
@@ -20,9 +32,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityVendorFoodItems extends AppCompatActivity {
 
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 101;
     private RecyclerView rcvFoodItems;
     private FoodMartApp foodMartApp;
     private ImageView imgFood;
@@ -32,36 +46,143 @@ public class ActivityVendorFoodItems extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_vendor_food_items);
+
     }
+
+    private void chooseImage(Context context) {
+
+        final CharSequence[] optionsMenu = {
+                "Take Photo", "Choose from Gallery"
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+
+        builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (optionsMenu[i].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+
+                } else if (optionsMenu[i].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, 1);
+                }
+            }
+        });
+        builder.show();
+    }
+
+    //Function to check Permission
+    public static boolean checkAndRequestPermissions(final Activity context) {
+        int WExtstorePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int cameraPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+        if (WExtstorePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(context, listPermissionsNeeded
+                            .toArray(new String[listPermissionsNeeded.size()]),
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    // Handled permission Result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS:
+                if (ContextCompat.checkSelfPermission(ActivityVendorFoodItems.this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(getApplicationContext(), "FlagUp Requires Access to Camara.", Toast.LENGTH_SHORT).show();
+                } else if (ContextCompat.checkSelfPermission(ActivityVendorFoodItems.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(getApplicationContext(), "FlagUp Requires Access to Your Storage.", Toast.LENGTH_SHORT).show();
+                } else {
+                    chooseImage(ActivityVendorFoodItems.this);
+                }
+                break;
+        }
+
+    }
+
+    //Handle Permission
+
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         foodMartApp = (FoodMartApp) getApplication();
-        arrayListFoodItems=new ArrayList<>();
+        imgFood = findViewById(R.id.IMG_FOOD);
+        arrayListFoodItems = new ArrayList<>();
         rcvFoodItems = findViewById(R.id.RCV_FOOD_ITEMS);
-        imgFood=findViewById(R.id.IMG_FOOD);
+
+
+        imgFood.setOnClickListener(view -> {
+
+            if (checkAndRequestPermissions(ActivityVendorFoodItems.this)) {
+                chooseImage(ActivityVendorFoodItems.this);
+            }
+//            Intent intentImgPicker=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+//            intentImgPicker.setType("image/*");
+//            intentImgPicker.putExtra("return-data", true);
+//            startActivityForResult(intentImgPicker,101);
+
+
+        });
 
         findViewById(R.id.BTN_ADD).setOnClickListener(view -> {
             BottomSheetBehavior.from(findViewById(R.id.BTMSHEET)).setState(BottomSheetBehavior.STATE_EXPANDED);
         });
 
-        imgFood.setOnClickListener(view -> {
-            Intent intentImgPicker=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-            intentImgPicker.setType("image/*");
-            intentImgPicker.putExtra("return-data", true);
-            startActivityForResult(intentImgPicker,12);
-        });
+
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        Uri imgPath=data.getData();
-        imgFood.setImageURI(imgPath);
-
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        imgFood.setImageBitmap(selectedImage);
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                imgFood.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+        //  Uri imgPath=data.getData();
+        //  imgFood.setImageURI(imgPath);
     }
+
+
 
     @Override
     protected void onResume() {
